a227d5a25206babe0f432d445e1974a57e0948db
AlvesAlbano <samir.saar@gmail.com>
2025-05-23 22:03:09 -0300

    update: project.clj
    atualizei para identificar a main no jar

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject api-nutricao "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://api-nutricao-production.up.railway.app"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-jetty-adapter "1.9.6"]]
   :plugins [[lein-ring "0.12.5"]]
+  :main api-nutricao.core
   :ring {:handler api-nutricao.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
