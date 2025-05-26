d6cb7542a101452a2d2819132d7ac5dfd76099ea
AlvesAlbano <samir.saar@gmail.com>
2025-05-25 22:32:30 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject api-nutricao "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://api-nutricao-production.up.railway.app"
+  :url ""
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
