6321e01c20e41f66fb4e3181d6bb49ba47d1d59b
DaviSilveira16 <158481047+DaviSilveira16@users.noreply.github.com>
2025-05-29 17:05:13 -0300

    Update core.clj

--- src/api_nutricao/core.clj
+++ src/api_nutricao/core.clj
@@ -1,7 +1,7 @@
 (ns api-nutricao.core
-  (:require [api-nutricao.handler :refer [app]]
-            [ring.adapter.jetty :refer [run-jetty]])
-  (:gen-class))
+     (:require [api-nutricao.handler :refer [app]]
+               [ring.adapter.jetty :refer [run-jetty]])
+     (:gen-class))
 
 (defn -main [& args]
   (run-jetty app {:port 3000 :join? false})
