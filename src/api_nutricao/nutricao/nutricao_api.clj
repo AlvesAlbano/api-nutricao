f6c824f560b70174da778555a2c249e78771a0b5
DaviSilveira16 <158481047+DaviSilveira16@users.noreply.github.com>
2025-06-01 12:20:45 -0300

    Update nutricao_api.clj

--- src/api_nutricao/nutricao/nutricao_api.clj
+++ src/api_nutricao/nutricao/nutricao_api.clj
@@ -1,9 +1,19 @@
 (ns api-nutricao.nutricao.nutricao-api
   (:require [clj-http.client :as client]
             [cheshire.core :as json]
-            [api-nutricao.traducao.traduzir-api :as trad]))
+            [api-nutricao.traducao.traduzir-api :as trad]
+            [clojure.string :as str]))
 
 (def api-key "wEs6WCnpgqM3QnNWYwQKBqzjch59uWB7Nye9VT0Q")
+
+(defn montar-descricao [alimento]
+  (let [desc (:description alimento)
+        categoria (:foodCategory alimento)
+        marca (:brandOwner alimento)
+        tipo (:dataType alimento)]
+    (->> [desc categoria marca tipo]
+         (remove nil?)
+         (str/join " - "))))
 
 (defn buscar-alimento-api [nome-pt]
   (let [nome-en (trad/portugues-ingles nome-pt)
@@ -12,10 +22,12 @@
                                                  "query" nome-en}
                                   :headers {"Accept" "application/json"}
                                   :as :json})
-        alimentos (get-in response [:body :foods])]
+        alimentos (get-in response [:body :foods])
+        alimentos-processados (mapv #(assoc % :descricao-detalhada (montar-descricao %))
+                                    alimentos)]
     {:status 200
      :headers {"Content-Type" "application/json"}
-     :body alimentos}))
+     :body alimentos-processados}))
 
 (def refeicoes-dia (atom []))
 
