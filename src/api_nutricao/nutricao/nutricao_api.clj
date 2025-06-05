98113f0de7955e610b7f00cdcb39fac4d0ce79e6
DaviSilveira16 <158481047+DaviSilveira16@users.noreply.github.com>
2025-06-05 19:26:59 -0300

    correção da api

--- src/api_nutricao/nutricao/nutricao_api.clj
+++ src/api_nutricao/nutricao/nutricao_api.clj
@@ -41,34 +41,3 @@
   {:status 200
    :headers {"Content-Type" "application/json"}
    :body (json/generate-string @refeicoes-dia)})
-
-(defn exibir-alimentos [alimentos]
-  (letfn [(mostrar [alimentos idx]
-            (if (or (empty? alimentos) (>= idx 7))
-              nil
-              (let [alimento (first alimentos)
-                    {:keys [description brandOwner servingSize foodCategory ingredients]} alimento
-                    calorias (get-in alimento [:foodNutrients 0 :value])]
-                (println (str idx ". " (clojure.string/upper-case description)
-                              " (Marca: " (or brandOwner "-") ")"))
-                (println (str "   Porção: " (or servingSize "Porção não informada")
-                              " | Calorias: " (or calorias "Calorias não informadas") " kcal"))
-                (println (str "   Categoria: " (or foodCategory "Categoria não informada")))
-                (println (str "   Ingredientes: " (or ingredients "Ingredientes não informados")))
-                (recur (rest alimentos) (inc idx)))))]
-    (mostrar alimentos 0)))
-
-(defn adicionar-refeicao-interativo []
-  (println "Digite o nome do alimento:")
-  (let [nome (read-line)
-        alimentos (buscar-alimento-api nome)
-        primeiros (take 7 alimentos)]
-    (exibir-alimentos primeiros)
-    (println "Selecione o número do alimento (0 a 6):")
-    (let [idx (Integer/parseInt (read-line))
-          alimento (nth primeiros idx nil)]
-      (if alimento
-        (do
-          (adicionar-refeicao alimento)
-          (println "Alimento adicionado com sucesso!"))
-        (println "Índice inválido.")))))
